71963f90846d849b3a39fda0c95eaaf305aad18a
jonghoonLaptop <jonghoone94@seoul.ac.kr>
2021-01-12 23:46:46 +0900

    C Intro

---
+++ C/Problems/02-1.c
@@ -0,0 +1,19 @@
+// 문제 1
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("홍길동\n홍 길 동\n홍  길  동\n");
+    return 0;
+}
+
+// 문제 2
+
+// int main(void)
+// {
+//     printf("옥종훈\n");
+//     printf("도쿄 츄오구\n");
+//     printf("010-0000-0000\n");
+//     return 0;
+// }
