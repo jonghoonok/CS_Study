696872c523679e90f8b7aa375308d64b8b81f569
jonghoonLaptop <jonghoone94@seoul.ac.kr>
2021-01-16 23:56:03 +0900

    C: Introduction

---
+++ C/Problems/08-2-2.c
@@ -0,0 +1,15 @@
+// AZ + ZA = 99인 모든 A, Z의 쌍을 구하자
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a, z;
+    
+    for(z = 1; z < 9; z++)
+    {
+        a = 9-z;
+        
+    }
+    return 0;
+}
