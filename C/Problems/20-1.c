dbd372ccd1e530c71765443d72d2888a0f2c62f6
jonghoonLaptop <jonghoone94@seoul.ac.kr>
2021-02-03 23:57:28 +0900

    C: Problems2

--- C/Problems/20-1.c
+++ C/Problems/20-1.c
@@ -15,8 +15,12 @@ void rotateArr(int (*arr)[4])
     }
     printf("\n");
 
-    arr = rotatedArr;
+    // arr = rotatedArr;
     // 이렇게 하면 함수 끝날 때 rotatedArr가 사라지기 때문에 arr가 가리키는 곳이 없어짐
+    // 배열 이름은 따로 저장되지 않으므로 배열 이름을 가리키는 포인터를 이용한 swap은 애초에 불가능
+    // 따라서 배열 내의 원소들을 일일이 바꿔주는 수밖에 없음
+    // 그리고 함수 내에서 바뀐 배열의 원소들은 함수가 끝나도 유지됨
+    // Call By Reference
 
 }
 
---
+++ C/Problems/20-2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int snail(int cnt, int rot, int n, int arr[][n])
+{
+    int i=rot, j=rot;
+    int len = n - (1 + 2 * rot);
+
+    if(len==0)
+    {
+        arr[i][j] = ++cnt;
+        return cnt;
+    }
+
+    for(;j<len;j++)
+        arr[i][j] = ++cnt;
+    for(;i<len;i++)
+        arr[i][j] = ++cnt;
+    for(;j>rot;j--)
+        arr[i][j] = ++cnt;
+    for(;i>rot;i--)
+        arr[i][j] = ++cnt;
+    
+    return cnt;
+}
+
+int main(void)
+{
+    int n = 0;
+    scanf("%d", &n);
+    int arr[n][n];
+
+    int cnt=0, rot=0;;
+    while(cnt < n*n)
+    {
+        cnt = snail(cnt, rot, n, arr);
+        rot++;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            printf("%2d", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
